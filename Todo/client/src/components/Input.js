90ef973cb03fbebeafa55056b3b427867106e85f
lateef-taiwo <taiwolateef55@gmail.com>
2022-12-20 17:22:08 +0100

    Added a new folder and modified package.json file

---
+++ Todo/client/src/components/Input.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+ 
+class Input extends Component {
+ 
+state = {
+action: ""
+}
+ 
+addTodo = () => {
+const task = {action: this.state.action}
+ 
+    if(task.action && task.action.length > 0){
+      axios.post('/api/todos', task)
+        .then(res => {
+          if(res.data){
+            this.props.getTodos();
+            this.setState({action: ""})
+          }
+        })
+        .catch(err => console.log(err))
+    }else {
+      console.log('input field required')
+    }
+ 
+}
+ 
+handleChange = (e) => {
+this.setState({
+action: e.target.value
+})
+}
+ 
+render() {
+let { action } = this.state;
+return (
+<div>
+<input type="text" onChange={this.handleChange} value={action} />
+<button onClick={this.addTodo}>add todo</button>
+</div>
+)
+}
+}
+ 
+export default Input
+
